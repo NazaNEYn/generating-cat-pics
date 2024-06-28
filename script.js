57cb02d5a31b77caa451b5ba17e9e025d58c7496
Nazanin Ashrafi <68081983+NazaNEYn@users.noreply.github.com>
2024-06-28 17:14:58 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const btn = document.querySelector(".btn");
+const img = document.querySelector(".img");
+const reset = document.querySelector(".reset");
+
+img.classList.add("hidden");
+
+btn.addEventListener("click", function () {
+  const random = Math.floor(Math.random() * 11) + 1;
+  console.log(random);
+
+  img.classList.remove("hidden");
+
+  img.src = `img/cat-${random}.avif`;
+});
+
+reset.addEventListener("click", function () {
+  img.classList.add("hidden");
+});
